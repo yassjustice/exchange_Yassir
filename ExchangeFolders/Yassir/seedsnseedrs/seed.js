5fb0b70067bfdd94fb2e970c62289a693e4c72ee
Yassir hakimi <yassirhakimi60@gmail.com>
2023-10-25 10:16:15 +0100

    seeds fixed

---
+++ ExchangeFolders/Yassir/seedsnseedrs/seed.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const { seedCustomers, seedCustomer } = require('./seedrs/customerSeed');
+// Import the seedUser function from seed.js
+const { seedUser, seedUsers } = require('./seedrs/userSeed');
+
+async function runSeed() {
+    try {
+        // Run the specified seed function (in this case, seedUser)
+        await seedUser();
+        console.log('Seed completed successfully.');
+    } catch (error) {
+        console.error('Error during seeding:', error);
+    }
+}
+
+// Execute the seed process
+// runSeed();
+async function runSeeds() {
+    try {
+        // Run the specified seed function (in this case, seedUser)
+        await seedUsers(10);
+        console.log('Seed completed successfully.');
+    } catch (error) {
+        console.error('Error during seeding:', error);
+    }
+}
+// runSeeds();
+
+
+async function runSeedCustom() {
+    try {
+        // Run the specified seed function (in this case, seedUser)
+        await seedCustomer();
+        console.log('Seed completed successfully.');
+    } catch (error) {
+        console.error('Error during seeding:', error);
+    }
+}
+
+async function runSeedsCustoms() {
+    try {
+        // Run the specified seed function (in this case, seedUser)
+        await seedCustomers(10);
+        console.log('Seed completed successfully.');
+    } catch (error) {
+        console.error('Error during seeding:', error);
+    }
+}
+runSeedCustom()
+runSeedsCustoms()
--- ExchangeFolders/Yassir/users/seed.js
+++
@@ -1,9 +0,0 @@
-async function runSeeds() {
-    try {
-        // Run the specified seed function (in this case, seedUser)
-        await seedUsers(10);
-        console.log('Seed completed successfully.');
-    } catch (error) {
-        console.error('Error during seeding:', error);
-    }
-}
